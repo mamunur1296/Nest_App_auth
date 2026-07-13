ae3fb496a2509dccef73d57c10eb06d207ccb1fe
mamunbbit <programer597@gmail.com>
2026-07-13 13:58:13 +0600

    Swagger: integrate Swagger for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth System API')
+    .setDescription('The Authentication API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
