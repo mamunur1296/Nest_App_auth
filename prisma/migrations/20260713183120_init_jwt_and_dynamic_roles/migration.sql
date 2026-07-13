40d92f194920892603fd3f8caefa9cdc593b1075
mamunbbit <programer597@gmail.com>
2026-07-14 00:33:31 +0600

    feat(database): add User and Role tables with required fields and foreign key constraints

---
+++ prisma/migrations/20260713183120_init_jwt_and_dynamic_roles/migration.sql
@@ -0,0 +1,55 @@
+/*
+  Warnings:
+
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `phoneNumber` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `roleId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT NOT NULL,
+ADD COLUMN     "lastName" TEXT NOT NULL,
+ADD COLUMN     "phoneNumber" TEXT NOT NULL,
+ADD COLUMN     "roleId" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_token_key" ON "RefreshToken"("token");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_userId_idx" ON "RefreshToken"("userId");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_token_idx" ON "RefreshToken"("token");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_expiresAt_idx" ON "RefreshToken"("expiresAt");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
